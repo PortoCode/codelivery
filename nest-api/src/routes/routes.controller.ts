9c2ef0d9ab36e9d3b18d737f06bedc1358b9b196
Rodrigo Porto <rodrigo.porto@watchguard.com>
2023-02-13 20:32:58 -0300

    adding kafka connection

--- nest-api/src/routes/routes.controller.ts
+++ nest-api/src/routes/routes.controller.ts
@@ -6,14 +6,24 @@ import {
   Patch,
   Param,
   Delete,
+  Inject,
+  OnModuleInit,
 } from '@nestjs/common';
 import { RoutesService } from './routes.service';
 import { CreateRouteDto } from './dto/create-route.dto';
 import { UpdateRouteDto } from './dto/update-route.dto';
+import { ClientKafka } from '@nestjs/microservices';
+import { Producer } from '@nestjs/microservices/external/kafka.interface';
 
 @Controller('routes')
-export class RoutesController {
-  constructor(private readonly routesService: RoutesService) {}
+export class RoutesController implements OnModuleInit {
+  private kafkaProducer: Producer;
+
+  constructor(
+    private readonly routesService: RoutesService,
+    @Inject('KAFKA_SERVICE')
+    private kafkaClient: ClientKafka,
+  ) {}
 
   @Post()
   create(@Body() createRouteDto: CreateRouteDto) {
@@ -40,9 +50,20 @@ export class RoutesController {
     return this.routesService.remove(+id);
   }
 
+  async onModuleInit() {
+    this.kafkaProducer = await this.kafkaClient.connect();
+  }
+
   @Get(':id/start')
   startRoute(@Param('id') id: string) {
-    console.log(id);
-    //enviar menssage pro kafka
+    this.kafkaProducer.send({
+      topic: 'route.new-direction',
+      messages: [
+        {
+          key: 'route.new-direction',
+          value: JSON.stringify({ routeId: id, clientId: '' }),
+        },
+      ],
+    });
   }
 }
